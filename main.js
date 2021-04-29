b8b02ca6dd43d4b8278aeae254d6f57936ca85f1
angelp495 <angeljpa95@gmail.com>
2021-04-29 10:45:28 -0500

    navBar & cards

---
+++ main.js
@@ -0,0 +1,37 @@
+const d = document,
+  $hiddenLink = d.querySelector(".a-to-show"),
+  $firedLink = d.querySelector(".show-a"),
+  $btnTeamA = d.getElementById("teamA"),
+  $btnTeamB = d.getElementById("teamB"),
+  $elTeamA = d.getElementsByClassName("teamA"),
+  $elTeamB = d.getElementsByClassName("teamB");
+
+d.addEventListener("mouseover", (e) => {
+  if (e.target.matches(".show-a") || e.target.matches(".show-a *")) {
+    $hiddenLink.style.display = "block";
+  }
+});
+
+d.addEventListener("mouseout", (e) => {
+  if (e.target.matches(".a-to-show") || e.target.matches(".a-to-show *")) {
+    $hiddenLink.style.display = "none";
+  }
+});
+
+d.addEventListener("click", (e) => {
+  if (e.target.id == "teamA") {
+    $elTeamB[0].style.visibility = "visible";
+    $elTeamB[1].style.visibility = "visible";
+
+    $elTeamA[0].style.visibility = "hidden";
+    $elTeamA[1].style.visibility = "hidden";
+  }
+
+  if (e.target.id == "teamB") {
+    $elTeamA[0].style.visibility = "visible";
+    $elTeamA[1].style.visibility = "visible";
+
+    $elTeamB[0].style.visibility = "hidden";
+    $elTeamB[1].style.visibility = "hidden";
+  }
+});
